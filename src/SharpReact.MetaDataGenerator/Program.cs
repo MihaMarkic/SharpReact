93efcc7bf84c9382dd546b1ef89f9d54aec74fc6
Miha Markič <miha@rthand.com>
2018-05-26 14:13:17 +0200

    Adds metadata generator project

---
+++ src/SharpReact.MetaDataGenerator/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SharpReact.MetaDataGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Settings
+    {
+
+    }
+
+    public class AssemblySettings
+    {
+        public string Path { get; set; }
+    }
+}
